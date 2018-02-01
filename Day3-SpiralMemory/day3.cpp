a6b8a394b1bbe49e84b4b771488a1c88a1217c70
hfors <hforsatgit@gmail.com>
2018-02-01 22:30:24 +0100

    Solve part two of Day 3.

--- Day3-SpiralMemory/day3.cpp
+++ Day3-SpiralMemory/day3.cpp
@@ -1,7 +1,20 @@
+/*
+This is a solution to both parts of the Day 3 puzzle of Advent of Code 2017.
+
+The problem description can be found at https://adventofcode.com/2017/day/3.
+*/
+
 #include <iostream>
 #include <cmath>
 #include <algorithm>
+#include <vector>
+#include <utility>
+
 int solve_part_one(int);
+int solve_part_two(int);
+
+std::pair<int, int> next_square(int, int, std::vector<std::vector<int>>&);
+std::vector<std::pair<int, int>> neighbours(int, int);
 
 int main()
 {
@@ -11,6 +24,7 @@ int main()
 	std::cin >> input;
 
 	std::cout << "The answer to part one is: " << solve_part_one(input) << '\n';
+	std::cout << "The answer to part two is: " << solve_part_two(input) << '\n';
 
 	return 0;
 }
@@ -37,4 +51,64 @@ int solve_part_one(int input)
 	const int y_cord = (side_len - 1) / 2  - corner_dist_min;
 
 	return x_cord + y_cord;
+}
+
+int solve_part_two(int input)
+{
+	std::size_t grid_size = 13;
+	std::vector<std::vector<int>> grid(grid_size, std::vector<int>(grid_size, 0));
+	int x = grid_size / 2, y = grid_size / 2;
+
+	grid[x][y] = 1;
+	grid[++x][y] = 1;
+
+	do {
+		auto p = next_square(x, y, grid);
+		x = p.first;
+		y = p.second;
+
+		for (auto& n : neighbours(x, y))
+			grid[x][y] += grid[n.first][n.second];
+	} while (grid[x][y] <= input);
+
+	return grid[x][y];
+}
+
+std::pair<int, int> next_square(int x, int y, std::vector<std::vector<int>>& grid)
+{
+	int del_x = x - grid.size() / 2, del_y = y - grid.size() / 2;
+	int dist = std::max(std::abs(del_x), std::abs(del_y));
+
+	if (del_y == -dist) {
+		if (del_x > -dist)
+			return std::make_pair(x - 1, y);
+		return std::make_pair(x, y + 1);
+	}
+	else if (del_x == -dist) {
+		if (del_y < dist)
+			return std::make_pair(x, y + 1);
+		return std::make_pair(x + 1, y);
+	}
+	else if (del_y == dist) {
+		if (del_x <= dist)
+			return std::make_pair(x + 1, y);
+	}
+
+	if (del_y > -dist)
+		return std::make_pair(x, y - 1);
+	return std::make_pair(x - 1, y);
+}
+
+std::vector<std::pair<int, int>> neighbours(int x, int y)
+{
+	std::vector<std::pair<int, int>> ngbs;
+	for (int i = -1; i <= 1; ++i) {
+		for (int j = -1; j <= 1; ++j) {
+			if (j == 0 && i == 0)
+				continue;
+			ngbs.push_back(std::make_pair(x + i, y + j));
+		}
+	}
+
+	return ngbs;
 }
