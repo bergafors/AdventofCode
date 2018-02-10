66e40dcc8edfa395326ff6a1ad34d1ff2f5c8e4e
hfors <hforsatgit@gmail.com>
2018-02-10 23:35:32 +0100

    Solve part one of Day 10.

--- Day10-Knot-Hash/day10.cpp
+++ Day10-Knot-Hash/day10.cpp
@@ -36,24 +36,39 @@ int main()
 
 std::pair<int, int> solve_both_parts(std::ifstream& ifs)
 {
-	std::vector<int> num_list(255);
+	std::vector<int> num_list(256);
 	std::iota(num_list.begin(), num_list.end(), 0);
 
-	auto pos = num_list.begin();
-	for (int skip = 0, length = 0; ifs >> length; ++skip) {
-		auto first = pos;
-		auto last = pos;
+	std::string line;
+	std::getline(ifs, line);
+	std::istringstream linestream(line);
 
-		if (auto dist = num_list.end() - 1 - first; dist >= length) {
-			last += length;
-			std::reverse(first, last);
+	auto pos = num_list.begin();
+	const auto b = num_list.begin();
+	const auto e = num_list.end();
+	std::string slength;
+	for (int skip = 0; std::getline(linestream, slength, ','); ++skip) {
+		const auto length = std::stoi(slength);
+		const auto nstep = length - 1;
+
+		auto first = pos;
+		const auto nstep_to_end = e - first;
+
+		auto last = nstep_to_end > nstep ? first + nstep : b + (nstep - nstep_to_end);
+		const auto next_pos_start = last + 1 == e ? b : last + 1;
+
+		for (int i = 0; i < length / 2; ++i) {
+			std::swap(*first, *last);
+			first = first + 1 == e ? b : first + 1;
+			last = last == b ? e - 1 : last - 1;
 		}
-		else {
-			last = num_list.begin() + length - dist;
-			while (first - last > 0)
-		}
-		pos += skip;
+
+		{
+			const auto nskip_to_end = e - next_pos_start;
+			pos = nskip_to_end > skip ? next_pos_start + skip : b + (skip - nskip_to_end);
+		} 
+		
 	}
 
-	return {0, 0};
+	return {num_list[0]*num_list[1], 0};
 }
