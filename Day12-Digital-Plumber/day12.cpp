4585ff203cac95d6edb057a0e5021cf9638853b6
hfors <hforsatgit@gmail.com>
2018-02-18 20:07:11 +0100

    Solve part one of Day 12.

--- Day12-Digital-Plumber/day12.cpp
+++ Day12-Digital-Plumber/day12.cpp
@@ -9,9 +9,11 @@ The problem description can be found at https://adventofcode.com/2017/day/12.
 #include <sstream>
 #include <string>
 #include <vector>
-#include <set>
+#include <unordered_set>
+#include <unordered_map>
 #include <ios>
 #include <algorithm>
+#include <list>
 
 int solve_part_one(std::ifstream&);
 int solve_part_two(std::ifstream&);
@@ -43,44 +45,67 @@ int main()
 
 int solve_part_one(std::ifstream& ifs)
 {
-	std::set<std::set<int>> groups;
+	std::unordered_map<int, std::unordered_set<int>> groups;
 	for (std::string line; std::getline(ifs, line);) {
-		std::vector<int> sub_group;
+		std::unordered_set<int> new_group;
 		std::istringstream linestream(line);
 
 		int leading_digit = 0;
 		linestream >> leading_digit;
-		sub_group.push_back(leading_digit);
+		new_group.insert(leading_digit);
 
 		linestream.seekg(4, std::ios::cur);
 		for (std::string digit; std::getline(linestream, digit, ',');)
-			sub_group.push_back(std::stoi(digit));
+			new_group.insert(std::stoi(digit));
 
-		// Find which, if any, existing groups this new sub group is connected to.
-		std::vector<decltype(groups)::iterator> connections;
-		std::for_each(sub_group.begin(), sub_group.end(), [&](auto digit) {
-			for (auto it = groups.begin(); it != groups.end(); ++it) {
-				if (auto itg = it->find(digit); itg != it->end())
-					connections.push_back(it);
-			}
-		});
+		groups[leading_digit].insert(new_group.begin(), new_group.end());
 
-
-		// Merge newly connected groups
-		if (connections.empty()) {
-			groups.insert(std::set<int>(sub_group.begin(), sub_group.end()));
-		} else {
-			auto merge_target = connections.back();
-			merge_target->insert(sub_group.begin(), sub_group.end());
-			for (auto it : connections) {
-				merge_target->insert(it->begin(), it->end());
-				groups.erase(it);
+		// Find which, if any, existing groups this new group is connected to
+		std::vector<int> connected_groups;
+		for (const auto& digit : groups[leading_digit]) {
+			for (const auto&[k, v] : groups) {
+				if (k == leading_digit)
+					continue;
+				if (auto pos = v.find(digit); pos != v.end())
+					connected_groups.push_back(k);
 			}
 		}
 
+		// Find the largest one the connected groups
+		auto max_group = leading_digit;
+		for (const auto& i : connected_groups) {
+			if (groups[i].size() > groups[max_group].size())
+				max_group = i;
+		}
+
+		// We merge to group[leading_digit]. By swapping the largest group here we ensure
+		// the minimal amount of copies and erases.
+		groups[leading_digit].swap(groups[max_group]);
+
+		// Merge the connected sets and erase the remaining copies
+		for (auto it = groups.begin(); it != groups.end();) {
+			auto& [k, v] = *it;
+
+			auto pos = std::find(connected_groups.begin(), connected_groups.end(), k);
+			if (pos != connected_groups.end()) {
+				groups[leading_digit].insert(v.begin(), v.end());
+				it = groups.erase(it);
+			}
+			else {
+				++it;
+			}
+		}
 	}
 
-	return 0;
+	std::size_t group_size = 0;
+	for (const auto& [k, v] : groups) {
+		if (auto it = v.find(0); it != v.end()) {
+			group_size = v.size();
+			break;
+		}
+	}
+
+	return group_size;
 }
 
 int solve_part_two(std::ifstream&)
