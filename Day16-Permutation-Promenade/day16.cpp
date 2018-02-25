33d09315a5254a6cdd99b25a7e59e751e644a4f2
hfors <hforsatgit@gmail.com>
2018-02-25 16:28:12 +0100

    Solve part two of Day 16.

--- Day16-Permutation-Promenade/day16.cpp
+++ Day16-Permutation-Promenade/day16.cpp
@@ -12,7 +12,8 @@ The problem description can be found at https://adventofcode.com/2017/day/16.
 #include <algorithm>
 
 std::string solve_part_one(std::ifstream&);
-int solve_part_two(std::ifstream&);
+std::string solve_part_two(std::ifstream&);
+void dance_round(std::list<char>&, std::ifstream&);
 
 int main()
 {
@@ -56,7 +57,51 @@ std::string solve_part_one(std::ifstream& file)
 	for (int i = 0; i < 16; ++i)
 		programs.emplace_back('a' + i);
 
-	int nspin = 0;
+	dance_round(programs, file);
+
+	std::string finished(programs.begin(), programs.end());
+	return finished;
+}
+
+std::string solve_part_two(std::ifstream& file)
+{
+	const std::size_t list_len = 16;
+	std::list<char> programs;
+	for (int i = 0; i < list_len; ++i)
+		programs.emplace_back('a' + i);
+
+	bool found_cycle = false;
+	unsigned long cycle_length;
+	unsigned long imax = 1000000000;
+	for (unsigned long i = 0; i < imax; ++i) {
+		dance_round(programs, file);
+		file.clear();
+		file.seekg(0, std::ifstream::beg);
+
+		// When (if) we find a cycle to the start we jump forward as many cycle lengths as possible
+		// One or more cycles are guaranteed to exist (as the number of possible list permutations is finite),
+		// although not one connecting to the start. 
+		// Looking for any cycle might the code faster on average, but it would entail searching over a
+		// large set of permutations. Luckily there was a cycle to the start in the input data provided for
+		// my account. YMMW.
+		if (!found_cycle) {
+			auto it = programs.begin();
+			for (int j = 0; j < list_len && *it == 'a' + j; ++j)
+				++it;
+			if (it == programs.end()) {
+				cycle_length = i + 1;
+				i = imax - 1 - imax % cycle_length;
+				found_cycle = true;
+			}
+		}
+	}
+
+	std::string finished(programs.begin(), programs.end());
+	return finished;
+}
+
+void dance_round(std::list<char>& programs, std::ifstream& file)
+{
 	for (std::string input; std::getline(file, input, ',');) {
 		std::istringstream linestream(input);
 
@@ -74,49 +119,41 @@ std::string solve_part_one(std::ifstream& file)
 			programs.splice(programs.begin(), programs, it, programs.end());
 			break;
 		}
-		case 'x': 
-			{
-				int a = 0, b = 0;
-				std::string num;
+		case 'x':
+		{
+			int a = 0, b = 0;
+			std::string num;
 
-				std::getline(linestream, num, '/');
-				a = std::stoi(num);
-				std::getline(linestream, num, '/');
-				b = std::stoi(num);
+			std::getline(linestream, num, '/');
+			a = std::stoi(num);
+			std::getline(linestream, num, '/');
+			b = std::stoi(num);
 
-				auto ita = programs.begin();
-				auto itb = programs.begin();
-				for (int i = 0; i < a; ++i)
-					++ita;
-				for (int i = 0; i < b; ++i)
-					++itb;
+			auto ita = programs.begin();
+			auto itb = programs.begin();
+			for (int i = 0; i < a; ++i)
+				++ita;
+			for (int i = 0; i < b; ++i)
+				++itb;
 
-				std::swap(*ita, *itb);
+			std::swap(*ita, *itb);
 
-			}
-			break;
+		}
+		break;
 		case 'p':
-			{
-				char a = 0, b = 0;
-				linestream.get(a);
-				linestream.get(b).get(b);
+		{
+			char a = 0, b = 0;
+			linestream.get(a);
+			linestream.get(b).get(b);
 
-				auto ita = std::find(programs.begin(), programs.end(), a);
-				auto itb = std::find(programs.begin(), programs.end(), b);
-				std::swap(*ita, *itb);
-			}
-			break;
+			auto ita = std::find(programs.begin(), programs.end(), a);
+			auto itb = std::find(programs.begin(), programs.end(), b);
+			std::swap(*ita, *itb);
+		}
+		break;
 		default:
 			std::cout << "ERROR\n";
 			break;
 		}
 	}
-
-	std::string finished(programs.begin(), programs.end());
-	return finished;
-}
-int solve_part_two(std::ifstream& file)
-{
-
-	return 0;
 }
